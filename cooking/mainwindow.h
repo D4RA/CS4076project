b1fac5357b7f916afd869afaebea27af236b5af1
D4RA <101725672+D4RA@users.noreply.github.com>
2023-03-22 19:31:27 +0000

    Add files via upload

--- cooking/mainwindow.h
+++ cooking/mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include "secondwindow.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,7 +15,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_radioButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    SecondWindow *secwin;
 };
 #endif // MAINWINDOW_H
