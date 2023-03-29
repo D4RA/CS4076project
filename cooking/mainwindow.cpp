acfc6e5f30659d73464026db78541c9d06ffb356
D4RA <101725672+D4RA@users.noreply.github.com>
2023-03-29 14:46:29 +0100

    Add files via upload

--- cooking/mainwindow.cpp
+++ cooking/mainwindow.cpp
@@ -7,6 +7,11 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap bkgnd("C:/Users/dara/OneDrive/Pictures/qt");
+       bkgnd = bkgnd.scaled(this->size(), Qt::IgnoreAspectRatio);
+       QPalette palette;
+       palette.setBrush(QPalette::Background, bkgnd);
+       this->setPalette(palette);
 }
 
 MainWindow::~MainWindow()
